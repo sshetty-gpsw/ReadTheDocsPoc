9c426fbb4570413e1f1597c09578cf3537f98b9a
sshetty-gpsw <sshetty@gopro.com>
2016-08-10 14:43:56 -0700

    Changed project Name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -243,7 +243,7 @@ html_static_path = ['_static']
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'pocdoc'
+htmlhelp_basename = 'Toolkitdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -311,7 +311,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'poc', u'poc Documentation',
+    (master_doc, 'Toolkit', u'Toolkit Documentation',
      [author], 1)
 ]
 
@@ -326,8 +326,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'poc', u'poc Documentation',
-     author, 'poc', 'One line description of project.',
+    (master_doc, 'Toolkit', u'Toolkit Documentation',
+     author, 'GoPro', 'Testing',
      'Miscellaneous'),
 ]
 
